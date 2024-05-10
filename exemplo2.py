0c339cd95c04c3681aec7935756ed4c765133f7d
Robert <Robert.freitasoliveira@hotmail.com>
2024-05-09 21:29:06 -0300

    calculador de calorias gasta por exercicio

---
+++ exemplo2.py
@@ -0,0 +1,13 @@
+def calcularCalorias(tempo, intensidade):
+    # Suponhamos que a queima de calorias seja 5 por minuto, e a intensidade seja um numero inteiro
+    caloriasPorMinuto = 5
+    caloriasTotais = tempo * caloriasPorMinuto * intensidade
+
+    return caloriasTotais
+
+# Exemplo de uso
+tempoAtividade = int(input("Digite o tempo de atividade: ")) # em minutos
+intensidadeAtividade = int(input('Digite a intensidade da atividade: ')) # fator de intensidade
+
+caloriasGastas = calcularCalorias(tempoAtividade, intensidadeAtividade)
+print(f"Você queimou {caloriasGastas} calorias durante a atividade")
